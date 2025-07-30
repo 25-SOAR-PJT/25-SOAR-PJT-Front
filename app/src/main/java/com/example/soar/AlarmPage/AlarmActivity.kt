0c8416b07b526ca56c5ffd7bd68a58310a1e40b9
ShinYoo-bin <sinddol07@naver.com>
2025-07-30 17:42:59 +0900

    feat: 알람 페이지 추가

---
+++ app/src/main/java/com/example/soar/AlarmPage/AlarmActivity.kt
@@ -0,0 +1,19 @@
+package com.example.soar.AlarmPage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.soar.R
+import com.example.soar.databinding.ActivityAlarmBinding
+
+class AlarmActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlarmBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAlarmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
