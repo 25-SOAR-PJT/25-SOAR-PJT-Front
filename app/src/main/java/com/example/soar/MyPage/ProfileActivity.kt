d8e1577cea9d15aa86e243a6cef01b430557e3af
ShinYoo-bin <sinddol07@naver.com>
2025-08-11 19:14:30 +0900

    feat: 프로필 페이지 레이아웃 구현

--- app/src/main/java/com/example/soar/MyPage/ProfileActivity.kt
+++ app/src/main/java/com/example/soar/MyPage/ProfileActivity.kt
@@ -1,21 +1,43 @@
 package com.example.soar.MyPage
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.soar.DetailPage.ReviewDetailActivity
 import com.example.soar.R
+import com.example.soar.databinding.ActivityPolicyBinding
+import com.example.soar.databinding.ActivityProfileBinding
+import com.example.soar.databinding.StepCsSummaryBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 앱 바
+        val textTitle = findViewById<TextView>(R.id.text_title)
+        textTitle.text = getString(R.string.profile)
+        findViewById<ImageView>(R.id.btn_back).setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        // TODO: 큐레이션 6번 처럼 정보 받아와서 각 부분에 view(사용자 정보) 만들어서 넣어야 함
+        // 각 view 부분 누르면 큐레이션의 해당 부분 가져와서 view 제목 같은거 몇개 안보이게 하고 수정하는 페이지로 대신 사용
+
+        binding.btnChangePw.setOnClickListener {
+            val intent = Intent(this, ChangePwActivity::class.java)
+            startActivity(intent)
         }
     }
 }
