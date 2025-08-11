bbc401cc1f71d697dcb3b6a57f6fa1a5212067b1
ShinYoo-bin <sinddol07@naver.com>
2025-08-11 19:14:20 +0900

    feat: 기록 페이지 레이아웃 구현

--- app/src/main/java/com/example/soar/MyPage/RecordActivity.kt
+++ app/src/main/java/com/example/soar/MyPage/RecordActivity.kt
@@ -1,21 +1,31 @@
 package com.example.soar.MyPage
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.soar.R
+import com.example.soar.databinding.ActivityCommentBinding
+import com.example.soar.databinding.ActivityRecordBinding
 
 class RecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_record)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 앱 바
+        val textTitle = findViewById<TextView>(R.id.text_title)
+        textTitle.text = getString(R.string.comment)
+        findViewById<ImageView>(R.id.btn_back).setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
         }
+
+        // TODO: recyclerview 만들어둠 (item_record_biz) 아답터 만들어야 함
     }
 }
